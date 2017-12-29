7be51632e0158379b889adf3a60a4df5fbde93a3
Roland Kay <roland.j.kay@googlemail.com>
2017-12-29 14:28:22 +0000

    Extend to all types of corner block

---
+++ README.md
@@ -0,0 +1,7 @@
+Corner blocks for Minetest
+==========================
+
+Minetest seems to be missing the corner blocks that are used with stairs. These
+appear automatically in Minecraft, but this doesn't seem to be the case in
+Minetest. They are useful for making the roofs of buildings. This simple demo
+mod adds these to the inventory.
--- corners/init.lua
+++ corners/init.lua
@@ -1,10 +1,28 @@
 
--- minetest.register_on_punchnode(
---    function(pos, node, puncher)
---             minetest.chat_send_all("Hey!!  Stop it!!  That hurts")
---    end
---)
+function string:split(sep)
+   local sep, fields = sep or ":", {}
+   local pattern = string.format("([^%s]+)", sep)
+   self:gsub(pattern, function(c) fields[#fields+1] = c end)
+   return fields
+end
 
+local function capitalizeFirst(word)
+  return string.upper(string.sub(word, 1, 1)) .. string.sub(word, 2)
+end
+
+-- Convert a machine readable name like "acacia_wood" into humane
+-- readable "Acacia Wood"
+--
+local function convertName(machine_name)
+  local words = machine_name:split("_")
+  
+  local new_words = {}
+  for n, word in ipairs(words) do
+    table.insert(new_words, capitalizeFirst(word))
+  end
+  
+  return table.concat(new_words, " ") 
+end
 
 local function rotate_and_place(itemstack, placer, pointed_thing)
     local p0 = pointed_thing.under
@@ -34,29 +52,61 @@ local function rotate_and_place(itemstack, placer, pointed_thing)
     return minetest.item_place(itemstack, placer, pointed_thing, param2)
 end
 
-
-minetest.register_node("corners:acacia", {
-    description = "Acacia Stair Corner",
-    paramtype2 = "facedir",
-    groups = {cracky = 3},
-    sounds = sound_stone,
-    tiles = {"default_acacia_wood.png"}, 
-    drawtype = "nodebox",
-    node_box = {
-        type = "fixed",
-        fixed = {
-            {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
-            {0, 0, 0, 0.5, 0.5, 0.5},
+local function register_corner(name, image_name)
+    if image_name == nil then 
+      image_name = "default_" .. name
+    end
+  
+    minetest.register_node("corners:" .. name, {
+        description = convertName(name) .. " Stair Corner",
+        paramtype2 = "facedir",
+        groups = {cracky = 3},
+        sounds = sound_stone,
+        tiles = {image_name .. ".png"}, 
+        drawtype = "nodebox",
+        node_box = {
+            type = "fixed",
+            fixed = {
+                {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+                {0, 0, 0, 0.5, 0.5, 0.5},
+            },
         },
-    },
-    on_place = function(itemstack, placer, pointed_thing)
-                   if pointed_thing.type ~= "node" then
-                       return itemstack
-                   end
+        on_place = function(itemstack, placer, pointed_thing)
+                       if pointed_thing.type ~= "node" then
+                           return itemstack
+                       end
 
-                   return rotate_and_place(itemstack, placer, pointed_thing)
-               end,
-})
+                       return rotate_and_place(itemstack, placer, pointed_thing)
+                   end,
+    })
+end
 
--- minetest.register_alias("corner", "corner_blocks:acacia")
+-- Register all corner types
+register_corner("brick")
+register_corner("acacia_wood")
+register_corner("aspen_wood")
+register_corner("bronze_block")
+register_corner("cobblestone", "default_cobble")
+register_corner("desert_cobblestone", "default_desert_cobble")
+register_corner("desert_sandstone")
+register_corner("desert_sandstone_brick")
+register_corner("desert_stone")
+register_corner("desert_stone_brick")
+register_corner("gold_block")
+register_corner("ice")
+register_corner("jungle_wood", "default_junglewood")
+register_corner("mossy_cobblestone", "default_mossycobble")
+register_corner("obsidian")
+register_corner("obsidian_brick")
+register_corner("pine_wood")
+register_corner("sandstone")
+register_corner("sandstone_brick")
+register_corner("silver_sandstone")
+register_corner("silver_sandstone_brick")
+register_corner("snow")
+register_corner("steel", "default_steel_block")
+register_corner("stone")
+register_corner("stone_brick")
+register_corner("straw", "farming_straw")
+register_corner("wooden", "default_wood")
 
